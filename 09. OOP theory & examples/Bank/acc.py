20479f5748894de655e254dc9a304ec8cfcb4d99
Lior <97247251+lior-abadi@users.noreply.github.com>
2022-03-09 16:02:14 -0300

    OOP update

--- 09. OOP theory & examples/Bank/acc.py
+++ 09. OOP theory & examples/Bank/acc.py
@@ -1,11 +1,44 @@
-
-
 class Account:
     
+    # This is the constructor of the class (similar to the constructor of smart contracts)
     def __init__(self, filepath):
-        with open(filepath, "r") as file:
+        self.filepath = filepath
+        with open(self.filepath, "r") as file:
             self.balance = float(file.read())
     
+    def withdraw(self, amount):
+        self.balance -= amount
+        self.commit()
+        print("Withdrawal Successful. New balance: ", self.balance)
     
-account = Account("balance.txt")
-print(account)        
+    def deposit(self, amount):
+        self.balance += amount    
+        self.commit()
+        print("Deposit Successful. New balance: ", self.balance)
+        
+    def commit(self):
+        with open(self.filepath, "w") as file:
+            file.write(str(self.balance))
+    
+
+# By passing to the child the parent class an inheritance is created. 
+class Checking(Account):
+    # This constructor structure establishes an inheritance of the Account class. Checking is a child of Account.
+    def __init__(self, filepath, fee):
+        self.fee = fee
+        Account.__init__(self, filepath)
+    
+    def transfer(self, amount, to):
+        if self.balance < amount:
+            print("Insufficient funds to make this transfer.")
+        else:     
+            self.balance -= (amount-self.fee)
+            print("Transfer successful. Sent: ", amount, " to: ", to, ". Current Balance: ", self.balance)
+            print("Fees charged: ", self.fee)
+            checking.commit()
+    
+    
+    
+checking = Checking("balance.txt")  
+checking.deposit(100)
+checking.transfer(1280, "Robertito")
