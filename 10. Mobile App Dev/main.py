3b22dba1837929dc46a092e9e3ce07422731981f
Lior <97247251+lior-abadi@users.noreply.github.com>
2022-03-11 10:24:18 -0300

    update kivy project

--- 10. Mobile App Dev/main.py
+++ 10. Mobile App Dev/main.py
@@ -10,6 +10,16 @@ Builder.load_file("design.kv")
 class LoginScreen(Screen):
     def sign_up(self):
         self.manager.current = "sign_up_screen"
+    
+    def login(self, uname, pword):
+        with open("users.json") as file:
+            users = json.load(file)
+            if uname in users and users[uname]["password"] == pword:
+                print("Login Successful")
+                self.manager.current = "login_screen_success"
+            else: 
+                self.ids.login_feedback.text = "Wrong username or password!"
+        
 
 class SignUpScreen(Screen):
     def add_user(self, uname, pword):
@@ -19,15 +29,26 @@ class SignUpScreen(Screen):
                         "created": datetime.now().strftime("%Y-%m-%d %H-%M-%S")}
         with open("users.json", "w") as file:
             json.dump(users, file)
-        self.manager.current = "sign_up_screen_success" 
-        
-class RootWidget(ScreenManager):
-    pass
+        self.manager.current = "sign_up_screen_success"  
 
 class SignUpScreenSuccess(Screen):
     def go_to_login_screen(self):
         self.manager.transition.direction = "right"
         self.manager.current = "login_screen"
+
+class SignUpScreenSuccess(Screen):
+    def go_to_login_screen(self):
+        self.manager.transition.direction = "right"
+        self.manager.current = "login_screen"
+        
+class LoginScreenSuccess(Screen):
+    def log_out(self):
+        self.manager.transition.direction = "right"
+        self.manager.current = "login_screen"
+
+
+class RootWidget(ScreenManager):
+    pass
 
 
 class MainApp(App):
