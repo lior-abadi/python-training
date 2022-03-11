49917644b648fff0f3e6427d9e8776a57d09cdb0
lior-abadi <97247251+lior-abadi@users.noreply.github.com>
2022-03-11 17:10:16 -0300

    Update scrapping.py

--- 11. WebScrapping/fundamentals/scrapping.py
+++ 11. WebScrapping/fundamentals/scrapping.py
@@ -1,9 +1,13 @@
 import requests
 from bs4 import BeautifulSoup
 
+# Get Website Linking and its Content
 r = requests.get("https://pythonizing.github.io/data/example.html")
 c = r.content
 
+# Parse and extract the parts of interest
 soup = BeautifulSoup(c, "html.parser")
-soup.find_all("div") 
+all = soup.find_all("div", {"class": "cities"}) 
 
+for item in all:
+    print(item.find_all("h2")[0].text)
