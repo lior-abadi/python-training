0f3e1da53b26883930a7049fffda4f303daa3e5a
Lior <97247251+lior-abadi@users.noreply.github.com>
2022-03-11 11:07:34 -0300

    kivy app update

--- 10. Mobile App Dev/main.py
+++ 10. Mobile App Dev/main.py
@@ -1,9 +1,11 @@
+import random
 from turtle import Screen
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen 
-import json
+import json, glob
 from datetime import datetime
+from pathlib import Path
 
 Builder.load_file("design.kv")
 
@@ -45,6 +47,17 @@ class LoginScreenSuccess(Screen):
     def log_out(self):
         self.manager.transition.direction = "right"
         self.manager.current = "login_screen"
+    
+    def pick_mood(self, mood):
+        available_feeligns = glob.glob("quotes/*txt")
+        available_feeligns = [Path(filename).stem for filename in available_feeligns]
+        if str(mood).lower() in available_feeligns:
+            with open(f"quotes/{str(mood).lower()}.txt", encoding= "utf8") as file:
+                quotes = file.readlines()
+            self.ids.mood_feedback.text = random.choice(quotes)
+        else:
+            self.ids.mood_feedback.text = "Mood not available yet, try another one!"    
+                
 
 
 class RootWidget(ScreenManager):
