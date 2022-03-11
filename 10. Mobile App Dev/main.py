8531d5e733c8c20fcc04018f423595e344521fe1
Lior <97247251+lior-abadi@users.noreply.github.com>
2022-03-10 22:18:42 -0300

    kivy app

---
+++ 10. Mobile App Dev/main.py
@@ -0,0 +1,24 @@
+from turtle import Screen
+from kivy.app import App
+from kivy.lang import Builder
+from kivy.uix.screenmanager import ScreenManager, Screen 
+
+Builder.load_file("design.kv")
+
+class LoginScreen(Screen):
+    def sign_up(self):
+        self.manager.current = "sign_up_screen"
+
+class SignUpScreen(Screen):
+    pass
+
+class RootWidget(ScreenManager):
+    pass
+
+
+class MainApp(App):
+    def build(self):
+        return RootWidget()
+
+if __name__ == "__main__":
+    MainApp().run()
