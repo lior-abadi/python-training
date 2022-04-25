b01f0787155a4608ffbdc5b6f7909ef021505249
Lior <97247251+lior-abadi@users.noreply.github.com>
2022-04-25 12:24:54 -0300

    Update dictionary.py

--- 12. Dictionary App/dictionary.py
+++ 12. Dictionary App/dictionary.py
@@ -1,15 +1,25 @@
 import json
-from difflib import SequenceMatcher
+from difflib import get_close_matches
 
 data = json.load(open("data.json"))
 
 def dictionary(w: str):
-
     w = w.lower()
-    if (w not in data):
-        return "Word not found"
+    resArray = get_close_matches(w, data.keys(), cutoff = 0.8)
 
-    return data[w]
+    # Evaluate perfect match.
+    if w in data:
+        return (f"{w}: {data[w]}")
+
+    # Evaluate word matches and mismatch.
+    if (len(resArray) > 0):
+        response = input(f"Did you mean instead {resArray[0]}, [Y/n]")
+        if response.lower() == "y":
+            return data[resArray[0]]
+        else: 
+            return "Okay, you can try another word."
+    else:
+        return "Word not found"
 
 word  = input("Enter word: ")
 
